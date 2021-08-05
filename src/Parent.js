b528fb6fbf0e4028db85850d263b2df8faa4f65a
owaisahmad870 <88275066+owaisahmad870@users.noreply.github.com>
2021-08-06 03:34:49 +0500

    Updated Comments

--- src/Parent.js
+++ src/Parent.js
@@ -11,3 +11,6 @@ function Parent(props) {
 }
 
 export default Parent;
+
+//The 'num' we given to App components is here available to us in the properties attribute
+//Point to ponder is we are not using the data in Parent in spite of being part of it and using the child componentfrom the origin of App component
