545bf9d40f4b9d2c8c060931fb6d66d8165d785b
owaisahmad870 <88275066+owaisahmad870@users.noreply.github.com>
2021-08-06 03:59:51 +0500

    Updated comments

--- src/App.js
+++ src/App.js
@@ -17,4 +17,6 @@ function App() {
 
 export default App;
 
-
+//In Value context file we have given the value 45 but here we have a provider giving the value 81
+//Because this wrapper is at the top level of hierarchy right after the root so all the comp inside this will acces this value including the parent
+//Despite of that in the parent comp we are not calling this value
