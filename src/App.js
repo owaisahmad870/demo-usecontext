018ba54096acf77c478c646bb88163adbf644094
owaisahmad870 <owaisahmad870@gmail.com>
2021-08-05 21:47:01 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Parent from './Parent';
+
+function App() {
+  let [number, setNumber] = useState(45);
+  return (
+    <div >
+      Hello World
+      <Parent num={number}></Parent>
+      <button onClick={()=>{ setNumber(++number) }} >Update Number</button>
+    </div>
+  );
+}
+
+export default App;
